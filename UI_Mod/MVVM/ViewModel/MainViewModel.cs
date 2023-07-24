335b239e4a19b315721ba74b828a241d016c865f
Lewandodev <mlewandowski380@gmail.com>
2023-07-24 23:23:01 +0200

    Further view option and panels update

---
+++ UI_Mod/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI_Mod.Core;
+
+namespace UI_Mod.MVVM.ViewModel
+{
+    internal class MainViewModel:ObservableObj
+    {
+        public MainViewModel()
+        {
+                
+        }
+    }
+}
