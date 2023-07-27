a49ceea4fa065f8303ca90186e72ad82e5d29097
Lewandodev <mlewandowski380@gmail.com>
2023-07-27 23:59:36 +0200

    Created Browse view option and its panel updated other

--- UI_Mod/MVVM/ViewModel/MainViewModel.cs
+++ UI_Mod/MVVM/ViewModel/MainViewModel.cs
@@ -9,7 +9,12 @@ namespace UI_Mod.MVVM.ViewModel
 {
     internal class MainViewModel:ObservableObj
     {
+
+        public RelayCommand OverViewCommand { get; set; }
+        public RelayCommand BrowseViewCommand { get; set; }
+
         public OverViewModel Over_V_M { get; set; }
+        public BrowseViewModel Browse_V_M { get; set; }
 
         private object _currentView;
 
@@ -24,7 +29,13 @@ namespace UI_Mod.MVVM.ViewModel
         public MainViewModel()
         {
             Over_V_M = new OverViewModel();
+            Browse_V_M = new BrowseViewModel();
+
             CurrentView = Over_V_M;
+
+            OverViewCommand=new RelayCommand(obj => { CurrentView = Over_V_M; });
+
+            BrowseViewCommand = new RelayCommand(obj => { CurrentView = Browse_V_M; });
 
         }
     }
