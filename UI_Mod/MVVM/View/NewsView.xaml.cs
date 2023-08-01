38677d717ae87d9c8b2b8927538a9605c3461fac
Lewandodev <mlewandowski380@gmail.com>
2023-08-01 23:59:21 +0200

    News View concept with clock

---
+++ UI_Mod/MVVM/View/NewsView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace UI_Mod.MVVM.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NewsView.xaml
+    /// </summary>
+    public partial class NewsView : UserControl
+    {
+
+        private DispatcherTimer timer;
+
+        public NewsView()
+        {
+            InitializeComponent();
+
+            timer= new DispatcherTimer();
+            timer.Interval=TimeSpan.FromSeconds(1);
+            timer.Tick+= Timer_Tick;
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ClockTxt.Text=DateTime.Now.ToString("G");
+        }
+    }
+}
--- UI_Mod/MVVM/ViewModel/MainViewModel.cs
+++ UI_Mod/MVVM/ViewModel/MainViewModel.cs
@@ -12,9 +12,11 @@ namespace UI_Mod.MVVM.ViewModel
 
         public RelayCommand OverViewCommand { get; set; }
         public RelayCommand BrowseViewCommand { get; set; }
+        public RelayCommand NewsViewCommand { get; set; }
 
         public OverViewModel Over_V_M { get; set; }
         public BrowseViewModel Browse_V_M { get; set; }
+        public NewsViewModel News_V_M { get; set; }
 
         private object _currentView;
 
@@ -30,12 +32,15 @@ namespace UI_Mod.MVVM.ViewModel
         {
             Over_V_M = new OverViewModel();
             Browse_V_M = new BrowseViewModel();
+            News_V_M = new NewsViewModel();
 
             CurrentView = Over_V_M;
 
             OverViewCommand=new RelayCommand(obj => { CurrentView = Over_V_M; });
 
             BrowseViewCommand = new RelayCommand(obj => { CurrentView = Browse_V_M; });
+
+            NewsViewCommand = new RelayCommand(obj => { CurrentView= News_V_M; });
 
         }
     }
