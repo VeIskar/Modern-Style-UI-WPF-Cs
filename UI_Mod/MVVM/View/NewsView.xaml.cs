9411603ec63515db24fa3717445705e98e2df62f
Lewandodev <mlewandowski380@gmail.com>
2023-08-04 23:22:57 +0200

    Image slider added in NewsView

--- UI_Mod/MVVM/View/NewsView.xaml.cs
+++ UI_Mod/MVVM/View/NewsView.xaml.cs
@@ -22,6 +22,9 @@ namespace UI_Mod.MVVM.View
     public partial class NewsView : UserControl
     {
 
+        private int index = 0;
+        SliderImage slideImgs = new SliderImage();
+
         private DispatcherTimer timer;
 
         public NewsView()
@@ -33,6 +36,11 @@ namespace UI_Mod.MVVM.View
             timer.Tick+= Timer_Tick;
             timer.Start();
 
+            slideImgs.AddImage("polit.png");
+            slideImgs.AddImage("factory.png");
+
+            imgSlide.Source = new BitmapImage(new Uri(slideImgs.imagesList[0], UriKind.Relative ) );
+
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -40,8 +48,51 @@ namespace UI_Mod.MVVM.View
             ClockTxt.Text=DateTime.Now.ToString("G");
         }
 
+
+        //left button (PREVIOUS -)
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+            if (index > 0)
+            {
+
+                index -= 1;
+                imgSlide.Source = new BitmapImage(new Uri(slideImgs.imagesList[index], UriKind.Relative));
+
+            }
+
+            
+        }
+
+        //right button (NEXT +)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (index < slideImgs.imagesList.Count-1)
+            {
+
+                index += 1;
+                imgSlide.Source = new BitmapImage(new Uri(slideImgs.imagesList[index], UriKind.Relative));
+
+            }
+
+            
+        }
+    }
+
+    class SliderImage
+    {
+        private string urlPath;
+        public List<string> imagesList;
+        public SliderImage()
+        {
+            imagesList = new List<string>();
+        }
+
+        //<Image Grid.Column="1" Source="/Images/polit.png" Height="323" Width="580" Stretch="UniformToFill" Margin="5" />
+        public void AddImage(string imgName)
+        {
+            urlPath = "/Images/"+imgName;
+            imagesList.Add(urlPath);
 
         }
     }
